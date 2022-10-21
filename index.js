5734945bf62e564366287ad3d745fffd640268ee
ukioxz <ikatember@gmail.com>
2022-10-21 22:42:01 +0300

    Add all information

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 (async () => {
 let response = await fetch('https://randomuser.me/api');
 
-let text = await response.json(); // прочитать тело ответа как текст
+let text = await response.json();
 let info1 = await text.results[0];
+let infoImg = await text.results[0].picture.large;
 let infoCity = await text.results[0].location.city;
 let infoPostcode = await text.results[0].location.postcode;
 let infoCell = await text.results[0].cell;
 let infoName = await text.results[0].name.first;
 console.log(text);
 console.log(info1);
+console.log(infoImg);
 console.log(infoCity);
 console.log(infoPostcode);
 console.log(infoCell);
 console.log(infoName);
+
+let elImg = document.getElementById('imgAva').src = infoImg;
+let elCity = document.getElementById('city').innerHTML = infoCity;
+let elPostcode = document.getElementById('postcode').innerHTML = infoPostcode;
+let elCell = document.getElementById('cell').innerHTML = infoCell;
+let elName = document.getElementById('name').innerHTML = infoName;
 })();
