1b97b66114a6a747558ae9e3b360fe9bd5a118a8
ukioxz <ikatember@gmail.com>
2022-10-23 12:59:03 +0300

    Formating

--- index.js
+++ index.js
@@ -1,101 +1,102 @@
+let containerUsers = document.getElementById('container');
+let btnClick = document.querySelector('.container2');
 
-  let containerUsers = document.getElementById('container');
-  let btnClick = document.querySelector('.container2');
+btnClick.addEventListener('click', () => {
+	containerUsers.style.visibility = 'visible';
+	btnClick.style.visibility = 'hidden';
+});
 
-  btnClick.addEventListener('click',() => {
-    containerUsers.style.visibility = "visible";
-    btnClick.style.visibility="hidden";
-  });
-  
 async function user1() {
-let response = await fetch('https://randomuser.me/api');
+	let response = await fetch('https://randomuser.me/api');
 
-let text = await response.json();
-let info1 = await text.results[0];
-let infoImg = await text.results[0].picture.large;
-let infoCity = await text.results[0].location.city;
-let infoPostcode = await text.results[0].location.postcode;
-let infoCell = await text.results[0].cell;
-let infoName = await text.results[0].name.first;
+	let text = await response.json();
+	let info1 = await text.results[0];
+	let infoImg = await text.results[0].picture.large;
+	let infoCity = await text.results[0].location.city;
+	let infoPostcode = await text.results[0].location.postcode;
+	let infoCell = await text.results[0].cell;
+	let infoName = await text.results[0].name.first;
 
-let elImg = document.getElementById('imgAva_1').src = infoImg;
-let elCity = document.getElementById('city_1').innerHTML = infoCity;
-let elPostcode = document.getElementById('postcode_1').innerHTML = infoPostcode;
-let elCell = document.getElementById('cell_1').innerHTML = infoCell;
-let elName = document.getElementById('name_1').innerHTML = infoName;
+	let elImg = document.getElementById('imgAva_1').src = infoImg;
+	let elCity = document.getElementById('city_1').innerHTML = infoCity;
+	let elPostcode = document.getElementById('postcode_1').innerHTML = infoPostcode;
+	let elCell = document.getElementById('cell_1').innerHTML = infoCell;
+	let elName = document.getElementById('name_1').innerHTML = infoName;
 }
 user1();
-async function user2(){
-let response = await fetch('https://randomuser.me/api');
 
-let text = await response.json();
-let info1 = await text.results[0];
-let infoImg = await text.results[0].picture.large;
-let infoCity = await text.results[0].location.city;
-let infoPostcode = await text.results[0].location.postcode;
-let infoCell = await text.results[0].cell;
-let infoName = await text.results[0].name.first;
+async function user2() {
+	let response = await fetch('https://randomuser.me/api');
 
-let elImg = document.getElementById('imgAva_2').src = infoImg;
-let elCity = document.getElementById('city_2').innerHTML = infoCity;
-let elPostcode = document.getElementById('postcode_2').innerHTML = infoPostcode;
-let elCell = document.getElementById('cell_2').innerHTML = infoCell;
-let elName = document.getElementById('name_2').innerHTML = infoName;
+	let text = await response.json();
+	let info1 = await text.results[0];
+	let infoImg = await text.results[0].picture.large;
+	let infoCity = await text.results[0].location.city;
+	let infoPostcode = await text.results[0].location.postcode;
+	let infoCell = await text.results[0].cell;
+	let infoName = await text.results[0].name.first;
+
+	let elImg = document.getElementById('imgAva_2').src = infoImg;
+	let elCity = document.getElementById('city_2').innerHTML = infoCity;
+	let elPostcode = document.getElementById('postcode_2').innerHTML = infoPostcode;
+	let elCell = document.getElementById('cell_2').innerHTML = infoCell;
+	let elName = document.getElementById('name_2').innerHTML = infoName;
 }
 user2();
 
-async function user3(){
-let response = await fetch('https://randomuser.me/api');
+async function user3() {
+	let response = await fetch('https://randomuser.me/api');
 
-let text = await response.json();
-let info1 = await text.results[0];
-let infoImg = await text.results[0].picture.large;
-let infoCity = await text.results[0].location.city;
-let infoPostcode = await text.results[0].location.postcode;
-let infoCell = await text.results[0].cell;
-let infoName = await text.results[0].name.first;
+	let text = await response.json();
+	let info1 = await text.results[0];
+	let infoImg = await text.results[0].picture.large;
+	let infoCity = await text.results[0].location.city;
+	let infoPostcode = await text.results[0].location.postcode;
+	let infoCell = await text.results[0].cell;
+	let infoName = await text.results[0].name.first;
 
-let elImg = document.getElementById('imgAva_3').src = infoImg;
-let elCity = document.getElementById('city_3').innerHTML = infoCity;
-let elPostcode = document.getElementById('postcode_3').innerHTML = infoPostcode;
-let elCell = document.getElementById('cell_3').innerHTML = infoCell;
-let elName = document.getElementById('name_3').innerHTML = infoName;
+	let elImg = document.getElementById('imgAva_3').src = infoImg;
+	let elCity = document.getElementById('city_3').innerHTML = infoCity;
+	let elPostcode = document.getElementById('postcode_3').innerHTML = infoPostcode;
+	let elCell = document.getElementById('cell_3').innerHTML = infoCell;
+	let elName = document.getElementById('name_3').innerHTML = infoName;
 }
 user3();
 
-async function user4(){
-let response = await fetch('https://randomuser.me/api');
+async function user4() {
+	let response = await fetch('https://randomuser.me/api');
 
-let text = await response.json();
-let info1 = await text.results[0];
-let infoImg = await text.results[0].picture.large;
-let infoCity = await text.results[0].location.city;
-let infoPostcode = await text.results[0].location.postcode;
-let infoCell = await text.results[0].cell;
-let infoName = await text.results[0].name.first;
+	let text = await response.json();
+	let info1 = await text.results[0];
+	let infoImg = await text.results[0].picture.large;
+	let infoCity = await text.results[0].location.city;
+	let infoPostcode = await text.results[0].location.postcode;
+	let infoCell = await text.results[0].cell;
+	let infoName = await text.results[0].name.first;
 
-let elImg = document.getElementById('imgAva_4').src = infoImg;
-let elCity = document.getElementById('city_4').innerHTML = infoCity;
-let elPostcode = document.getElementById('postcode_4').innerHTML = infoPostcode;
-let elCell = document.getElementById('cell_4').innerHTML = infoCell;
-let elName = document.getElementById('name_4').innerHTML = infoName;
+	let elImg = document.getElementById('imgAva_4').src = infoImg;
+	let elCity = document.getElementById('city_4').innerHTML = infoCity;
+	let elPostcode = document.getElementById('postcode_4').innerHTML = infoPostcode;
+	let elCell = document.getElementById('cell_4').innerHTML = infoCell;
+	let elName = document.getElementById('name_4').innerHTML = infoName;
 }
 user4();
-async function user5(){
-let response = await fetch('https://randomuser.me/api');
 
-let text = await response.json();
-let info1 = await text.results[0];
-let infoImg = await text.results[0].picture.large;
-let infoCity = await text.results[0].location.city;
-let infoPostcode = await text.results[0].location.postcode;
-let infoCell = await text.results[0].cell;
-let infoName = await text.results[0].name.first;
+async function user5() {
+	let response = await fetch('https://randomuser.me/api');
 
-let elImg = document.getElementById('imgAva_5').src = infoImg;
-let elCity = document.getElementById('city_5').innerHTML = infoCity;
-let elPostcode = document.getElementById('postcode_5').innerHTML = infoPostcode;
-let elCell = document.getElementById('cell_5').innerHTML = infoCell;
-let elName = document.getElementById('name_5').innerHTML = infoName;
+	let text = await response.json();
+	let info1 = await text.results[0];
+	let infoImg = await text.results[0].picture.large;
+	let infoCity = await text.results[0].location.city;
+	let infoPostcode = await text.results[0].location.postcode;
+	let infoCell = await text.results[0].cell;
+	let infoName = await text.results[0].name.first;
+
+	let elImg = document.getElementById('imgAva_5').src = infoImg;
+	let elCity = document.getElementById('city_5').innerHTML = infoCity;
+	let elPostcode = document.getElementById('postcode_5').innerHTML = infoPostcode;
+	let elCell = document.getElementById('cell_5').innerHTML = infoCell;
+	let elName = document.getElementById('name_5').innerHTML = infoName;
 }
 user5();
